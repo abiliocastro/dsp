ae99b4ed76a80af3403059ec88e5daa2cdbbe419
Abilio Castro <castro.abilio@gmail.com>
2020-11-28 20:52:29 -0300

    Finalizando questao1

--- dsp_erexercice_list01/src/com/ufc/br/Exercise01.java
+++ dsp_erexercice_list01/src/com/ufc/br/Exercise01.java
@@ -7,52 +7,77 @@ import java.util.Scanner;
 
 public class Exercise01 {
 
+    private static Scanner lerTeclado = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner lerTeclado = new Scanner(System.in);
         String nomeArquivo = lerTeclado.nextLine();
         String n1s = lerTeclado.nextLine();
         String n2s = lerTeclado.nextLine();
         if(!n1s.equals("") && !n2s.equals("")) { // Quando o caba coloca os dois números
-            validaNumeros(n1s, n2s);
-            Scanner lerArquivo = getScanner(nomeArquivo);
-            if (lerArquivo == null) return;
-            int n1 = Integer.valueOf(n1s);
-            int n2 = Integer.valueOf(n2s);
-            int count = 1;
-            while (lerArquivo.hasNextLine()) {
-                String linha = lerArquivo.nextLine();
-                if(count >= n1 && count <= n2) {
-                    System.out.println(linha);
-                    if(count == n2) return;
-                }
-                count++;
-            }
+            processaComDoisNumeros(nomeArquivo, n1s, n2s);
         } else if(!n1s.equals("")) { // Quando o caba coloca só o primeiro número
-            validaNumero(n1s);
-            Scanner lerArquivo = getScanner(nomeArquivo);
-            if (lerArquivo == null) return;
-            int n = Integer.valueOf(n1s);
-            int count = 1;
-            while (lerArquivo.hasNextLine()) {
-                String linha = lerArquivo.nextLine();
-                if(count >= n) System.out.println(linha);
-                count++;
-            }
+            processaComPrimeiroNumero(nomeArquivo, n1s);
         } else if(!n2s.equals("")) { // Quando o caba coloca só o segundo número
-            validaNumero(n2s);
-            Scanner lerArquivo = getScanner(nomeArquivo);
-            if (lerArquivo == null) return;
-            int n = Integer.valueOf(n2s);
-            int count = 1;
-            while (lerArquivo.hasNextLine()) {
-                String linha = lerArquivo.nextLine();
-                if(count <= n) System.out.println(linha);
-                count++;
-            }
+            processaComSegundoNumero(nomeArquivo, n2s);
         } else { // Quando o caba é senvergoin e coloca nenhum número
             System.out.println("Por favor informe pelo menos um valor");
+            lerTeclado.close();
             return;
         }
+    }
+
+    private static void processaComDoisNumeros(String nomeArquivo, String n1s, String n2s) {
+        validaNumeros(n1s, n2s);
+        Scanner lerArquivo = getScanner(nomeArquivo);
+        if (lerArquivo == null) return;
+        int n1 = Integer.valueOf(n1s);
+        int n2 = Integer.valueOf(n2s);
+        int count = 1;
+        while (lerArquivo.hasNextLine()) {
+            String linha = lerArquivo.nextLine();
+            if(count >= n1 && count <= n2) {
+                System.out.println(linha);
+                if(count == n2) return;
+            }
+            count++;
+        }
+        fecharScanners(lerArquivo);
+        return;
+    }
+
+    private static void processaComPrimeiroNumero(String nomeArquivo, String n1s) {
+        validaNumero(n1s);
+        Scanner lerArquivo = getScanner(nomeArquivo);
+        if (lerArquivo == null) return;
+        int n = Integer.valueOf(n1s);
+        int count = 1;
+        while (lerArquivo.hasNextLine()) {
+            String linha = lerArquivo.nextLine();
+            if(count >= n) System.out.println(linha);
+            count++;
+        }
+        fecharScanners(lerArquivo);
+        return;
+    }
+
+    private static void processaComSegundoNumero(String nomeArquivo, String n2s) {
+        validaNumero(n2s);
+        Scanner lerArquivo = getScanner(nomeArquivo);
+        if (lerArquivo == null) return;
+        int n = Integer.valueOf(n2s);
+        int count = 1;
+        while (lerArquivo.hasNextLine()) {
+            String linha = lerArquivo.nextLine();
+            if(count <= n) System.out.println(linha);
+            count++;
+        }
+        fecharScanners(lerArquivo);
+        return;
+    }
+
+    private static void fecharScanners(Scanner lerArquivo) {
+        lerArquivo.close();
+        lerTeclado.close();
     }
 
     private static void validaNumeros(String n1s, String n2s) {
